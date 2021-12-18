81d5de6c41428105cbb05d950b8ed07402989d0d
VincentVegaW <kev1@ro.ru>
2021-12-18 21:03:08 +0200

    home work1

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Ekaterina Kichigina on 18.12.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+ 
+
+}
